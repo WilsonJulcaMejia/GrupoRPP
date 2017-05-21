614d6479bcf1570b0b7aeb7bf7412eb420ed8be7
Luis Enrique Campos Rubina <lucaru9@gmail.com>
2017-05-20 21:40:56 -0500

    Backend

---
+++ Desarrollo/AINNI/Back/rpp/news/views.py
@@ -0,0 +1,15 @@
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import AllowAny
+from .models import New
+from .paginations import StandardResultsSetPagination
+from .serializers import ListNewSerializer
+
+
+class ListNewsAPI(ListAPIView):
+    serializer_class = ListNewSerializer
+    authentication_classes = ()
+    permission_classes = (AllowAny,)
+    pagination_class = StandardResultsSetPagination
+
+    def get_queryset(self):
+        return New.objects.filter(is_enabled=True,)
