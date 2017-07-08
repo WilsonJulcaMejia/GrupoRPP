6c43a5c2f2e842a4001db94df272171cd785d4b3
wilson <wiljm0.0@gmail.com>
2017-07-08 14:26:00 -0500

    update paquete liberacion and add file in .sh

---
+++ Libreria de release/paquete-liberacion.bat
@@ -0,0 +1,15 @@
+@echo off
+COPY "..\Libreria de trabajo\Documentos\Formato de pase a produccion.xlsx" "Formato de pase a produccion.xlsx"
+set NOMBRE_PROYECTO=AINNI
+set FECHA_ACTUAL=%DATE%
+set ANIO=%FECHA_ACTUAL:~6,4%
+set MES=%FECHA_ACTUAL:~3,2%
+set DIA=%FECHA_ACTUAL:~0,2%
+set RUTA=%NOMBRE_PROYECTO%_%DIA%%MES%%ANIO%
+MD %RUTA%
+cd %RUTA% 
+MD Documentos
+MD Scripts
+MD Ejecutable
+MD Rollback
+exit
